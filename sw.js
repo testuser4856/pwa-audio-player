0c56d2d074ff39082107ced519c68354273ac10a
testuser4856 <ichiyabozu@gmail.com>
2025-10-20 13:43:09 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION='v2002';
+const VERSION='v2003';
 const CACHE=`pwa-audio-mini-${VERSION}`;
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting(); });
